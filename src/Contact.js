2e40a4faed9a99d2ec010200a58fd8ec5b420136
Kanchan Laptop <digitalnomadexpenses@gmail.com>
2023-10-24 18:44:30 +0530

    interection observer added for about-us, contact, questions. both desktop and mobile version, including SVGstext animation

--- src/Contact.js
+++ src/Contact.js
@@ -1,26 +1,43 @@
 import React from "react"
 import Form from "./Form"
+import { useInView } from 'react-intersection-observer'
 
 
 export default function Contact(props) {
+
+	//intersection observer lazy loading or animation start
+	const { ref: contactRef, inView: contactVisibility } = useInView();
+ 	//intersection observer lazy loading or animation start
+
 	const [formDarkMode, setFormDarkMode] = React.useState(props.darkMode)
 	return(
-		<div className="container pt-5">
+		<div 
+		className="container pt-5"
+		ref={contactRef}>
 			<div className={props.darkMode ? "contact dark" : "contact light"}>
 				<div className="d-flex flex-row p-5">
-					<div className="col-lg-6 col-5 pt-lg-5 pt-2">
+					<div className={`col-lg-6 col-5 pt-lg-5 pt-2 ${contactVisibility ? "contact-div-animation" : ""}`}>
 						<div className="contact-heading pb-4">Let's Work<span> Together</span></div>
 						<div className="contact-text pe-5 pb-5">
 							We're thrilled that you're considering us for your 
 							web development needs. Whether you have 
 							a project in mind, questions about our services
 						</div>
-						<div className="contact-heading-timings pb-4 pt-1">How To <span>Reach </span>Us</div>
-						<div className="contact-text-timings pe-5 pb-2">Phone:Your Phone Number</div>
-						<div className="contact-text-timings pe-5 pb-2">Email:inquires@arrowcodedevelopers.com</div>
-						<div className="contact-text-timings pe-5 pb-2">Monday - Friday: 9:00 AM to 5 PM IST </div>
-					</div>
-					<div className="col">
+						<div 
+							className={`contact-heading-timings pb-4 pt-1 ${contactVisibility ? "ctt-1-animation" : ""}`}>
+							How To <span>Reach </span>Us
+						</div>
+						<div 
+							className={`contact-text-timings pe-5 pb-2 ${contactVisibility ? "ctt-2-animation" : ""}`}>
+							Phone:Your Phone Number</div>
+						<div 
+							className={`contact-text-timings pe-5 pb-2 ${contactVisibility ? "ctt-3-animation" : ""}`}>
+							Email:inquires@arrowcodedevelopers.com</div>
+						<div 
+							className={`contact-text-timings pe-5 pb-2 ${contactVisibility ? "ctt-4-animation" : ""}`}>
+							Monday - Friday: 9:00 AM to 5 PM IST </div>
+						</div>
+					<div className={contactVisibility ? "contact-form-animation col" : "col"}>
 						<Form 
 							formDarkMode={props.darkMode}
 						/>
